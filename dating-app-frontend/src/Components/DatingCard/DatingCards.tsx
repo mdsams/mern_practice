8b717a92dbf1b3827049bfde1c8fcbcf3c84eed9
mdsams <tabrejmdsams786@gmail.com>
2021-11-30 15:35:33 +0530

    Ready To Deploy

--- dating-app-frontend/src/Components/DatingCard/DatingCards.tsx
+++ dating-app-frontend/src/Components/DatingCard/DatingCards.tsx
@@ -1,22 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import DatingCard from 'react-tinder-card';
-import './DatingCards.css';
+import './DatingCards.css'
+import axios from '../axios'
+
+interface person{
+    name: string,
+    imgUrl: string
+}
 
 const DatingCards = () => {
-    const [people, setPeople] = useState([
-        {   name: "Random Guy",
-            imgUrl: "https://images.unsplash.com/photo-1520409364224-63400afe26e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=658&q=80" 
-        },
-        {   name: "Another Guy",
-            imgUrl: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80"
-        },
-        {   name: "Random Girl",
-            imgUrl: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80"
-        },
-        {   name: "Another Girl",
-            imgUrl: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+    const [people, setPeople] = useState([])
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get("/dating/cards")
+            setPeople(req.data)
         }
-    ])
+        fetchData()
+    },[])
     const swiped = (direction: string, nameToDelete: string) => {
         console.log("receiving" + nameToDelete)
     }
@@ -27,7 +27,7 @@ const DatingCards = () => {
     return (
         <div className="datingCards">
             <div className="datingCards_container">
-                {people.map((person) => (
+                {people.map((person: person) => (
                     <DatingCard
                         className="swipe"
                         key={person.name}
